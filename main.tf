fa4cf5900463db21dfc1cdb7b6a53d37cb6e86b1
Tom Burge <tom@tomburge.org>
2023-11-05 22:01:22 -0600

    updated logic again

--- main.tf
+++ main.tf
@@ -35,10 +35,18 @@ resource "aws_s3_bucket_metric" "this" {
 #   )
 # }
 
+# resource "aws_s3_bucket_policy" "policy" {
+#   bucket = aws_s3_bucket.this.id
+#   policy = var.bucket_policy != null ? var.bucket_policy : (
+#     var.kms_key_config != null && var.kms_key_config.key_arn != null ? data.aws_iam_policy_document.kms_encryption.json :
+#     data.aws_iam_policy_document.default_encryption.json
+#   )
+# }
+
 resource "aws_s3_bucket_policy" "policy" {
   bucket = aws_s3_bucket.this.id
   policy = var.bucket_policy != null ? var.bucket_policy : (
-    var.kms_key_config != null && var.kms_key_config.key_arn != null ? data.aws_iam_policy_document.kms_encryption.json :
+    var.kms_key_config != null ? data.aws_iam_policy_document.kms_encryption.json :
     data.aws_iam_policy_document.default_encryption.json
   )
 }
